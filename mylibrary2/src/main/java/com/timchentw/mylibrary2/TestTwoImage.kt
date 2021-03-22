4acb564e1dceb7d8142079698e631c1bffedf199
TimChen <tim@riversense.com>
2021-03-22 11:10:18 +0800

    feat: add TestTwoImage

---
+++ mylibrary2/src/main/java/com/timchentw/mylibrary2/TestTwoImage.kt
@@ -0,0 +1,12 @@
+package com.timchentw.mylibrary2
+
+import android.content.Context
+import androidx.appcompat.widget.AppCompatImageView
+
+/**
+ *  @author Tim Chen
+ *  @time   2021/3/22
+ *  @desc
+ */
+class TestTwoImage(context: Context) : AppCompatImageView(context) {
+}
